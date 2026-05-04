e0ff2f52a976e4c5f0e72d344b8dd0b063170d75
YinCY <yinchunyou2012@163.com>
2026-05-04 19:34:43 +0800

    learnCpp

---
+++ learnCpp/helloworld/main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+// int main()
+// {
+//     std::cout << "Hello, World!" << std::endl;
+//     return 0;
+// }
+
+// int main()
+// {
+//     std::cout << 5;
+//     std::cout << -6.7;
+//     std::cout << 'H';
+//     std::cout << "Hello";
+
+//     return 0;
+// }
+
+
+int main()
+{
+    std::cout << "Enter three numbers: ";
+    int x {};
+    int y {};
+    int z {};
+    std::cin >> x >> y >> z;
+    std::cout << "You entered: " << x << ", " << y << ", and " << z << ".\n";
+    
+    return 0;
+}
