924b4a50e8532e3928c86e50e6ca56a6fb68bdea
YinCY <yinchunyou2012@163.com>
2026-05-06 22:44:58 +0800

    cpp

--- learnCpp/function/main.cpp
+++ learnCpp/function/main.cpp
@@ -46,19 +46,124 @@
 // }
 
 
-int getValueFromUser()
-{
-    std::cout << "enter an integer: ";
-    int input{};
-    std::cin >> input;
+// int getValueFromUser()
+// {
+//     std::cout << "enter an integer: ";
+//     int input{};
+//     std::cin >> input;
 
+//     return input;
+// }
+
+// int main()
+// {
+//     int num { getValueFromUser() };
+//     std::cout << num << " doubled is: " << num * 2 << "\n";
+
+//     return 0;
+// }
+
+// =========== non-value return function ==========
+
+// void printHi()
+// {
+//     std::cout << "Hi\n";
+//     return; // optional, but can be used to exit the function early, tell compiler to return to the caller
+// }
+
+// int main()
+// {
+//     printHi();
+
+//     return 0;
+// }
+
+// void returnNothing()
+// {
+
+// }
+
+// int returnFive()
+// {
+//     return 5;
+// }
+
+// int main()
+// {
+//     returnNothing();
+//     returnFive();
+
+//     std::cout << returnFive() << "\n";
+//     // std::cout << returnNothing() << "\n";
+
+//     return 0;
+// }
+
+// return a value from void function is a compile error
+
+
+// =========== function parameters and arguments ==========
+
+// int getValueFromUser()
+// {
+//     std::cout << "Enter an integer: ";
+//     int input{};
+//     std::cin >> input;
+
+//     return input;
+// }
+
+// void printDouble(int value)
+// {
+//     std::cout << value << " doubled is: " << value * 2 << '\n';
+// }
+
+// int main()
+// {
+//     printDouble( getValueFromUser() );
+//     return 0;
+// }
+
+
+// int add(int x, int y)
+// {
+//     return x + y;
+// }
+
+// int multiply(int z, int w)
+// {
+//     return z * w;
+// }
+
+// int main()
+// {
+//     std::cout << add(4, 5) << '\n';
+//     std::cout << add(1+2, 3*4) << '\n';
+
+//     int a{ 5 };
+//     std::cout << add(a, a) << '\n';
+
+//     std::cout << add(1, multiply(2, 3)) << '\n';
+//     std::cout << add(1, add(2, 3)) << '\n';
+
+//     return 0;
+// }
+
+int getValuesFromUser()
+{
+    int input;
+    std::cout << "enter an integer: ";
+    std::cin >> input;
     return input;
+}
+
+int doubleNumber()
+{
+    return 2 * getValuesFromUser();
 }
 
 int main()
 {
-    int num { getValueFromUser() };
-    std::cout << num << " doubled is: " << num * 2 << "\n";
-
-    return 0;
+    int x { doubleNumber() };
+    std::cout << "Doubled is: " << x << '\n';
 }
