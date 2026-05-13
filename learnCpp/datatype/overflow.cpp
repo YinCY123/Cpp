f77fe14361a1cbc6ba5619d8b53537c1a407e694
YinCY <yinchunyou2012@163.com>
2026-05-13 22:43:52 +0800

    datatype

---
+++ learnCpp/datatype/overflow.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+
+// int main()
+// {
+//     int x { 2147483647 };
+//     std::cout << x << '\n';
+
+//     x = x + 1;
+//     std::cout << x << '\n';
+
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     std::cout << -8 /5 << '\n';
+
+//     return 0;
+// }
+
+
+// int main()
+// {
+//     unsigned short x { 65535 };
+//     std::cout << x << '\n';
+
+//     x = 65536;
+//     std::cout << x << '\n';
+
+//     x = 65537;
+//     std::cout << x << '\n';
+
+//     return 0;
+// }
+
+
+int main()
+{
+    unsigned short x { 0 };
+    std::cout << x << '\n';
+
+    x = -1;
+    std::cout << x << '\n';
+
+    x = -2;
+    std::cout << x << '\n';
+
+    return 0;
+}
